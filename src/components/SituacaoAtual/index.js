c185035da96b0e8467a8958d3a4c290eac52aa0d
ericsonmoreira <moreira.ericson@gmail.com>
2020-05-16 17:44:02 -0300

    versão 2

--- src/components/SituacaoAtual/index.js
+++ src/components/SituacaoAtual/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 
 import './style.css'
 
-
 // data => { confirmados:0, suspeitos:0, descartados:0, letais:0 }
 function SituacaoAtual({ data }) {
   return (
     <div className="SituacaoAtual">
-      Situação atual:
+      <span>Situação Atual</span>
       <ul>
-        <li>Confirmados: {data.confirmados }</li>
-        <li>Suspeitos: {data.suspeitos }</li>
-        <li>Descartados: {data.descartados }</li>
-        <li>Letais: {data.letais }</li>
+        <li id="confirmados">Confirmados: {data.confirmados }</li>
+        <li id="suspeitos">Suspeitos: {data.suspeitos }</li>
+        <li id="descartados">Descartados: {data.descartados }</li>
+        <li id="letais">Letais: {data.letais }</li>
       </ul>
     </div>
   );
