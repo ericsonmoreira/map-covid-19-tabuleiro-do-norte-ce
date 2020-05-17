b2c9cfb55739c8c7851e01cfba61b16c5350fe52
ericsonmoreira <moreira.ericson@gmail.com>
2020-05-17 15:20:11 -0300

    dados atualizados em 17/05/2020

---
+++ src/components/Info/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import { Icon } from 'leaflet';
+
+function Info() {
+  return (
+    <div>
+      <Icon></Icon>
+    </div>
+  )
+}
+
+export default Info;
